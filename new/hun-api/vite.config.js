a69c8c88d6fbda721d06dd29503a0ac9ac1d6a05
ivg-home-win <ivangeliumsan@yandex.ru>
2025-05-22 21:04:37 +0300

    Третья стадия, пытаюсь починить после того как всё сломалось

--- new/hun-api/vite.config.js
+++ new/hun-api/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/index.html'),
+      },
+    },
+  },
+  server: {
+    port: 5173,
+  },
 })
